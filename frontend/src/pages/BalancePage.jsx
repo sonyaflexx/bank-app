5912262384324bbd7e76ff72e804164010ecfc20
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-04-06 23:11:15 +0700

    TODO немного раскидал на подключение к бэку

--- frontend/src/pages/BalancePage.jsx
+++ frontend/src/pages/BalancePage.jsx
@@ -4,13 +4,15 @@ import HistoryList from "../components/history/HistoryList";
 import BackButton from "../components/buttons/BackButton";
 
 export default function BalancePage() {
+    const balance = "1 000" // TODO запрос на бэк на получение баланса
+
     return (
         <div className="relative flex items-center flex-col bg-white w-full mx-20 pt-7 pb-8 px-10 rounded-2xl shadow-xl">
             <Header backHref="/"/>
             <div className="flex flex-col gap-7 w-full">
                 <div className="flex flex-col items-center gap-1 bg-gray-20 py-5 rounded-lg">
                     <p className="font-semibold text-lg">Ваш баланс</p>
-                    <p className="text-4xl font-bold text-green-180">100 ₽</p>
+                    <p className="text-4xl font-bold text-green-180">{balance} ₽</p>
                 </div>
                 <div className="flex flex-col gap-2">
                     <p className="flex items-center text-lg gap-1 w-full">
