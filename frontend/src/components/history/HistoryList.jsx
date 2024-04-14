ce8c7fb43b83a09c18a9a534fc2f7646c22db508
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-04-15 02:47:16 +0700

    Добавил "История пуста"

--- frontend/src/components/history/HistoryList.jsx
+++ frontend/src/components/history/HistoryList.jsx
@@ -4,6 +4,9 @@ import HistoryItem from "./HistoryItem";
 export default function HistoryList(props) {
     return (
         <div className="d max-h-96 overflow-y-auto pr-2">
+            {props.transactions.length === 0 &&
+                <div className="w-full justify-center text-center text-sm p-4 text-gray-200">История пуста</div>
+            }
             {props.transactions.map((obj) =>
                 <HistoryItem 
                     transaction_type={obj.transaction_type}
