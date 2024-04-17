5403ffddec44961ab1bff2c3b78ee3f3d2537aa6
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-04-17 18:35:35 +0700

    переделал под версел

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://bank-app-frontend-delta.vercel.app',
     credentials: true,
     methods: ["GET", "POST"]
 }))
