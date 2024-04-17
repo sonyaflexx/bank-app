813df3f36335b3ef019d91ac4a84fe940d311013
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-04-17 18:44:51 +0700

    vercel

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://bank-app-backend-7x3fwf0qx-sonyaflexxs-projects.vercel.app";
 
 const api = axios.create({
   withCredentials: true,
