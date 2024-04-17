3cdb443e3f4b30eb7453bb72ea924f1ad35e9f67
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-04-17 19:27:40 +0700

    vercel

--- backend/db.config.js
+++ backend/db.config.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+const pg = require('pg');
 
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize("postgres://default:DMBRjJS7KL1Z@ep-wispy-hall-a46mq5hr-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require", {
-  dialect: 'postgres'
+  dialectModule: pg
 });
 
 module.exports = sequelize;
