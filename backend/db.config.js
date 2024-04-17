422c9f5b59dc765f5b8b4787b79294693ccfa198
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-04-17 19:20:05 +0700

    vercel

--- backend/db.config.js
+++ backend/db.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+const sequelize = new Sequelize("postgres://default:DMBRjJS7KL1Z@ep-wispy-hall-a46mq5hr-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require", {
   dialect: 'postgres'
 });
 
